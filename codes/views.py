88394b6d6df1cf7bf88870e3ab41f7862aa6f91f
nandanv99 <nandanv99@gmail.com>
2022-03-05 16:17:35 +0530

    new codes for login added

--- codes/views.py
+++ codes/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from .models import exp, newcodes
 from django.core.mail import send_mail
 from django.conf import settings
+from django.contrib.auth.hashers import make_password,check_password
 
 # Google sheets api imports
 from googleapiclient.discovery import build
@@ -76,9 +77,9 @@ def mailapi(request):
 
 def register(request):
     name=request.GET.get('name')
-    email=request.GET.get('logemail')
+    # email=request.GET.get('logemail')
     password=request.GET.get('logpass')
-    print(name,email,password)
+    print(name,password)
     SAMPLE_SPREADSHEET_ID = '1dR1QxQfCFWSL5PIe5a6xoVP5m2fzZgzQpawRkUN-jdE'
     try:
         service = build('sheets', 'v4', credentials=creds)
@@ -89,12 +90,17 @@ def register(request):
         print(values)
     except HttpError as err:
         print(err)
-
-    dic=[[name,email,password]]
+    dic=[[name,password]]
+    dic1=[[name]]
 
     request = sheet.values().append(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                                     range="details!A1:B500", 
                                     valueInputOption="USER_ENTERED",
+                                    body={"values":dic1}).execute()
+    request = sheet.values().append(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range="passwords!A1:B500", 
+                                    valueInputOption="USER_ENTERED",
                                     body={"values":dic}).execute()
+
     print("Succesfull added")
     return render(request,"register.html")
